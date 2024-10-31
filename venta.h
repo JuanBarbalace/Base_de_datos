70748ca2c64079a8bb7eb84782c035fa2f93f63c
JuanBarbalace <100445382+JuanBarbalace@users.noreply.github.com>
2024-10-31 16:01:33 -0300

    Add files via upload
    Agregué la clase fechas a estas 2 clases

--- venta.h
+++ venta.h
@@ -1,47 +1,47 @@
-// Venta.h
-#ifndef VENTA_H
-#define VENTA_H
-
+//Venta.h
+#pragma once
 #include <string>
 #include <vector>
+#include "Fecha.h"
 
 using namespace std;
 
 class Venta {
-    private:
+private:
     int _idVenta;
     int _idCliente;
     int _idEmpleado;
     Fecha _fecha;
     float _total;
 
-    public:
-    //Constructor
-    Venta(int idV, int idC, int idE, Fecha fecha, float total): _idVenta(idV), _idCliente(idC), _idEmpleado(idE), _fecha(fecha), _total(total) {}
+public:
+    // Constructor
+    Venta(int idV, int idC, int idE, Fecha fecha, float total)
+        : _idVenta(idV), _idCliente(idC), _idEmpleado(idE), _fecha(fecha), _total(total) {}
 
-    //Getters
-    int getIdVenta()     { return _idVenta; }
-    int getIdCliente()   { return _idCliente; }
-    int getIdEmpleado()  { return _idEmpleado; }
-    Fecha getFecha()     { return _fecha; }
-    float getTotal()     { return _total; }
+    // Getters
+    int getIdVenta() const { return _idVenta; }
+    int getIdCliente() const { return _idCliente; }
+    int getIdEmpleado() const { return _idEmpleado; }
+    Fecha getFecha() const { return _fecha; }
+    float getTotal() const { return _total; }
 
-    //Setters
+    // Setters
     void setIdVenta(int idVenta) { _idVenta = idVenta; }
     void setIdCliente(int idCliente) { _idCliente = idCliente; }
     void setIdEmpleado(int idEmpleado) { _idEmpleado = idEmpleado; }
     void setFecha(Fecha fecha) { _fecha = fecha; }
     void setTotal(float total) { _total = total; }
 
-    //calcula el total de ventas por mes
-    float totalVentasXMes(Venta* ventas, int numVentas, int mes, int anio) {
+    //calcular el total de ventas por mes
+    static float totalVentasXMes(Venta* ventas, int numVentas, int mes, int anio) {
         float totalVentas = 0.0;
-        for (int i=0;i<numVentas;++i) {
-            if (ventas[i].getFecha().getMes()==mes && ventas[i].getFecha().getAnio() == anio) {
+        for (int i = 0; i < numVentas; ++i) {
+            if (ventas[i].getFecha().getMes() == mes && ventas[i].getFecha().getAnio() == anio) {
                 totalVentas += ventas[i].getTotal();
             }
         }
-    return totalVentas;
+        return totalVentas;
     }
 };
 
